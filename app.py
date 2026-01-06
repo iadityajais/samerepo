659aed0c2227616ac863e644f4350cd7a4462325
iadityajais <iasadityajais@gmail.com>
2026-01-06 14:45:20 +0530

    pythoncode

---
+++ app.py
@@ -0,0 +1,17 @@
+# Simple Python program
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Python 🐍"
+
+def add_numbers(a, b):
+    return a + b
+
+# Main execution
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
+
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    print("Sum is:", add_numbers(x, y))
+
